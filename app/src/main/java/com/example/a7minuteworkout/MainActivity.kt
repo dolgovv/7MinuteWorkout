c5e4c727eb56ffc42f5e964b8171f8625534503d
Dolgovv <ogodolgov@gmail.com>
2022-08-28 15:25:28 +0300

    First take
    Just started everything. Made a main screen UI template. Gonna fix the ripple effect and continue doing everything else

--- app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkout/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llStart.setOnClickListener{
+            Toast.makeText(this, "lol", Toast.LENGTH_SHORT).show()
+        }
     }
 }
